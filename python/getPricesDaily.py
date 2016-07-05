f94b9505b233f247026dc91f030c2f81ee23c821
Ric Holland <ric.holland89@gmail.com>
2016-07-05 21:10:40 +0100

    Can now track multiple stocks. Visualization script zooms in to latest day.

--- python/getPricesDaily.py
+++ python/getPricesDaily.py
@@ -3,38 +3,49 @@
 import urllib2, json, time, re
 
 dataLoc = "/home/ric/dev/stocks/python/data/"
+lastTimeDict = {}
 
-def getPrice(lastTime = ""):
+def getPrice(ltDict, *codes):
     """Function to get price"""
 
-    url = "http://finance.google.com/finance/info?client=ig&q=LON:BA"
+    res = []
+    tmp = ""
+    for code in codes:
+        tmp = tmp + "," + str(code)
+    tmp = tmp[1:]
+    url = "http://finance.google.com/finance/info?client=ig&q=" + tmp
     txt = urllib2.urlopen(url).read()
-    js = json.loads(txt[3:])[0]
-#    print(js)
-#    for x in js.keys():
-#        print(str(x))
-    changeTime = str(js["lt_dts"])
-    if lastTime != changeTime:
-        price = float(js["l"])
-        return (changeTime, price)
-    else:
-        print("No change in price")
+    jsArr = json.loads(txt[3:])
+    for js in jsArr:
+        changeTime = str(js["lt_dts"])
+        stockId = str(js["e"]) + ":" + str(js["t"])
+        if stockId not in ltDict or ltDict[stockId] != changeTime:
+            price = float(js["l"])
+            res.append((stockId, changeTime, price))
+        else:
+            print("No change in price")
+    if res:
+        return res
 
-def toFile(results):
-    with open(dataLoc + "data.csv", "a") as f:
+def toFile(results, stock):
+    with open(dataLoc + stock + "_data.csv", "a") as f:
         time, price = results
         time = re.sub(r"[A-Z]", " ", time).strip()
         f.write("{0},{1}\n".format(time, price))
     f.close
 
-t, p = getPrice()
-toFile((t, p))
-print((t, p))
+for line in getPrice(lastTimeDict, "LON:BA", "LON:FDM"):
+    sid, t, p = line
+    toFile((t, p), sid)
+    print((sid, t, p))
+    lastTimeDict[sid] = t
 
 while time.localtime().tm_hour < 18:
-    tup = getPrice(t)
-    if tup:
-        print(tup)
-        toFile(tup)
-        t = tup[0]
+    lst = getPrice(lastTimeDict, "LON:BA", "LON:FDM")
+    if lst:
+        for tup in lst:
+            print(tup)
+            sid, t, p = tup
+            toFile((t, p), sid)
+            lastTimeDict[sid] = t
     time.sleep(30)
--- python/vis.py
+++ python/vis.py
@@ -1,22 +1,31 @@
 import csv
-from datetime import datetime
+from datetime import datetime, date
 from bokeh.plotting import figure, output_file, show
 
 x = []
 y = []
 
-with open("/home/ric/dev/stocks/python/data.csv") as data:
+stock = "LON:BA"
+
+with open("/home/ric/dev/stocks/python/data/" + stock + "_data.csv") as data:
     dd = csv.reader(data, delimiter=",")
     for row in dd:
         t, p = row
-        print "{0}\t{1}".format(t, p)
+        #print "{0}\t{1}".format(t, p)
         x.append(datetime.strptime(t, "%Y-%m-%d %H:%M:%S"))
         y.append(p)
+    # Add current time with last known price
+    x.append(datetime.now())
+    y.append(p)
 
-plot = figure(title = "BAE", x_axis_type = "datetime")
+today = date.today()
+x_f = datetime(today.year, today.month, today.day, 8, 30)
+x_l = datetime(today.year, today.month, today.day, 18)
+
+plot = figure(title = stock, x_axis_type = "datetime", x_range = [x_f, x_l])
 plot.line(x, y)
 
 show(plot)
 
-for z in x:
-    print(z)
+#for z in x:
+    #print(z)
